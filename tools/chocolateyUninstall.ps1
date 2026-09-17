2b09d146a6bce2435b5dac84f219c68157271a8d
filzyx <contact@filzyx.com>
2026-09-17 17:47:29 +0500

    Update tools/chocolateyUninstall.ps1

---
+++ tools/chocolateyUninstall.ps1
@@ -0,0 +1,17 @@
+$ErrorActionPreference = 'Stop'
+
+$softwareName = 'Duplicate File Finder*'
+[array]$key = Get-UninstallRegistryKey -SoftwareName $softwareName
+
+if ($key.Count -eq 1) {
+  $key | ForEach-Object {
+    $file = $_.UninstallString
+    $silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+    Uninstall-ChocolateyPackage -PackageName $env:ChocolateyPackageName -FileType exe -SilentArgs $silentArgs -File $file
+  }
+} elseif ($key.Count -eq 0) {
+  Write-Warning "$softwareName has already been uninstalled by other means."
+} else {
+  Write-Warning "$($key.Count) matches found for $softwareName - aborting to avoid uninstalling the wrong software."
+  $key | ForEach-Object { Write-Warning "- $($_.DisplayName)" }
+}
